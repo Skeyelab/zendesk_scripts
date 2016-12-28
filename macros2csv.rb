34c275333fc09b074711016881cee1bb41f53f90
Eric Dahl <eric@skeyelab.com>
2016-12-28 15:00:44 -0600

    comments

--- macros2csv.rb
+++ macros2csv.rb
@@ -15,22 +15,29 @@ client = ZendeskAPI::Client.new do |config|
   config.retry = true
 end
 
-
+#declare and array to store macros in
 macro_array = []
+
+#loop thru every macro in the desk
 client.macros.all do |macro|
+  #declaue an array for this particular macro
   row =[]
+  #store the macro id into the row
   row << macro.id
 
+  #loop thru each macro.action, looking for the comment_value action
   macro.actions.each do |action|
-
     if action.field == "comment_value"
+      #when we find it, add the action.value to the row
       row << action.value
     end
   end
+
+  #add the individual macro to the macro_array
   macro_array << row
 end
 
-
+#loop thru the macro_array, writing each row to a csv file
 CSV.open("macros.csv", "wb") do |csv|
   macro_array.each do |row|
     csv << row
